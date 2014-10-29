bec7bbe0bfd4e79e73adaebbd7a00e3f296cb59e
Rob Dimsdale <robert.dimsdale@gmail.com>
2014-10-28 22:43:59 -0700

    Add osHelper sleep test.

--- oshelper_test.go
+++ oshelper_test.go
@@ -1,6 +1,8 @@
 package garagepi_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -21,6 +23,15 @@ var _ = Describe("OsHelper", func() {
 			resp, err := osHelper.Exec("echo", "hello", "world")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(resp).To(Equal("hello world\n"))
+		})
+	})
+
+	Describe("Sleep", func() {
+		It("Sleeps for at least the time provided", func() {
+			start := time.Now()
+			osHelper.Sleep(1 * time.Second)
+			end := time.Now()
+			Expect((end.Sub(start)).Seconds()).Should(BeNumerically(">", 1))
 		})
 	})
 })
