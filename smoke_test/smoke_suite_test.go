13cc7c033ecc30edfaaaf4689759c05136824d1f
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-08-20 21:32:19 -0700

    Add smoke tests.
    - do not run smoke-tests as part of unit tests.
    
    [#101008598]

--- smoke_test/smoke_suite_test.go
+++ smoke_test/smoke_suite_test.go
@@ -1,18 +1,19 @@
-package main_test
+package smoke_test
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
 
 	"testing"
 )
 
-func TestGaragepi(t *testing.T) {
+func TestSmokeGaragepi(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "GaragepiExecutable Suite")
+	RunSpecs(t, "Garagepi Smoke Tests Suite")
 }
 
 var (
@@ -26,14 +27,12 @@ var _ = BeforeSuite(func() {
 	// handling requests.
 	SetDefaultEventuallyTimeout(10 * time.Second)
 
-	var err error
-	garagepiBinPath, err = gexec.Build("github.com/robdimsdale/garagepi", "-race")
-	Expect(err).ShouldNot(HaveOccurred())
+	garagepiBinPath = os.Getenv("GARAGEPI_BIN_PATH")
+	if garagepiBinPath == "" {
+		fmt.Printf("garagepiBinPath not set - using 'garagepi'")
+		garagepiBinPath = "garagepi"
+	}
 
 	httpPort = uint(59990 + 2*GinkgoParallelNode())
 	httpsPort = uint(59991 + 2*GinkgoParallelNode())
-})
-
-var _ = AfterSuite(func() {
-	gexec.CleanupBuildArtifacts()
 })
---
+++ smoke_test/smoke_test.go
@@ -0,0 +1,168 @@
+package smoke_test
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+func startMainWithArgs(args ...string) *gexec.Session {
+	command := exec.Command(garagepiBinPath, args...)
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gbytes.Say("garagepi starting"))
+	return session
+}
+
+func validateSuccessAnyLengthBody(resp *http.Response, err error) {
+	Expect(err).NotTo(HaveOccurred())
+	Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+	validateBody(resp, true)
+}
+
+func validateSuccessNonZeroLengthBody(resp *http.Response) {
+	Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+	validateBody(resp, false)
+}
+
+func validateBody(resp *http.Response, anySize bool) {
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).NotTo(HaveOccurred())
+
+	if anySize {
+		Expect(len(body)).Should(BeNumerically(">=", 0))
+	} else {
+		Expect(len(body)).Should(BeNumerically(">", 0))
+	}
+}
+
+var _ = Describe("GaragepiExecutable", func() {
+	var (
+		session *gexec.Session
+		args    []string
+	)
+
+	BeforeEach(func() {
+		args = []string{}
+	})
+
+	AfterEach(func() {
+		session.Terminate()
+	})
+
+	Describe("request handling", func() {
+		BeforeEach(func() {
+			args = append(args, "-dev")
+		})
+
+		Context("when enableHTTPS is true", func() {
+			BeforeEach(func() {
+				args = append(args, "-enableHTTPS=true")
+				args = append(args, fmt.Sprintf("-httpsPort=%d", httpsPort))
+			})
+
+			Context("when both -certFile and -keyFile are provided", func() {
+				var (
+					keyFile  string
+					certFile string
+
+					client *http.Client
+				)
+
+				BeforeEach(func() {
+					testDir := getDirOfCurrentFile()
+					fixturesDir := filepath.Join(testDir, "..", "fixtures")
+					keyFile = filepath.Join(fixturesDir, "key.pem")
+					certFile = filepath.Join(fixturesDir, "cert.pem")
+
+					args = append(args, "-keyFile="+keyFile)
+					args = append(args, "-certFile="+certFile)
+
+					// Load client cert
+					cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					// Load CA cert
+					caCert, err := ioutil.ReadFile(certFile)
+					if err != nil {
+						log.Fatal(err)
+					}
+					caCertPool := x509.NewCertPool()
+					caCertPool.AppendCertsFromPEM(caCert)
+
+					// Setup HTTPS client
+					tlsConfig := &tls.Config{
+						Certificates: []tls.Certificate{cert},
+						RootCAs:      caCertPool,
+					}
+					tlsConfig.BuildNameToCertificate()
+					transport := &http.Transport{TLSClientConfig: tlsConfig}
+					client = &http.Client{Transport: transport}
+				})
+
+				It("accepts HTTPS connections", func() {
+					session = startMainWithArgs(args...)
+					Eventually(session).Should(gbytes.Say("garagepi started"))
+
+					resp, err := client.Get(fmt.Sprintf("https://localhost:%d/", httpsPort))
+					Expect(err).NotTo(HaveOccurred())
+					validateSuccessNonZeroLengthBody(resp)
+				})
+
+				Context("when enableHTTP is true", func() {
+					BeforeEach(func() {
+						args = append(args, "-enableHTTP=true")
+						args = append(args, fmt.Sprintf("-httpPort=%d", httpPort))
+					})
+
+					Context("when forceHTTPS is true", func() {
+						BeforeEach(func() {
+							args = append(args, "-forceHTTPS=true")
+						})
+
+						It("redirects HTTP to HTTPS", func() {
+							session = startMainWithArgs(args...)
+							Eventually(session).Should(gbytes.Say("garagepi started"))
+
+							req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:%d/", httpPort), nil)
+							Expect(err).NotTo(HaveOccurred())
+
+							transport := http.Transport{}
+							resp, err := transport.RoundTrip(req)
+
+							Expect(resp.StatusCode).To(Equal(http.StatusFound))
+
+							expectedLocation := fmt.Sprintf("localhost:%d", httpsPort)
+
+							location, err := resp.Location()
+							Expect(err).NotTo(HaveOccurred())
+							Expect(location.Scheme).To(Equal("https"))
+							Expect(location.Host).To(Equal(expectedLocation))
+						})
+					})
+				})
+			})
+		})
+	})
+})
+
+func getDirOfCurrentFile() string {
+	_, filename, _, _ := runtime.Caller(1)
+	return path.Dir(filename)
+}
